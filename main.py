c7b99736a505cc2bb6be96816d3b9691aed777b3
Jonathan Sneh <jonathaninterweb@gmail.com>
2022-02-26 01:17:40 -0600

    updated mains

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data = pd.read_excel("../data/Map_the_Meal_Gap_Data/MMG2021_2019Data_ToShare.xlsx", sheet_name=1)
-cleaned_data = data[["FIPS", "2019 Food Insecurity Rate"]]
-
-cleaned_data = cleaned_data.rename(columns={"2019 Food Insecurity Rate":"FIR"})
-# FIR = Food Insecurity Rate
-cleaned_data.to_json("../data/processed/2019_food_insecurity.json", orient="records")
