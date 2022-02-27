c8e6d0f3ee081434575bc52d18893bf5e337ffd2
Devak Nanda <devaknanda@hotmail.com>
2022-02-27 00:39:56 -0600

    tmp branch to do merge resolve 😭

--- frontend/src/Overlay.js
+++ frontend/src/Overlay.js
@@ -6,9 +6,13 @@ import { Loader } from '@googlemaps/js-api-loader';
 
 const Overlay = (props) => {
     let [imgUrl, setImgUrl] = useState("");
-    
+    console.log(props);
     useEffect(
         () => {
+            console.log("ss");
+            /*if (!props.visible) {
+                return;
+            }*/
             console.log("he");
             const fetchLoader = async () => {
                 const loader = new Loader({apiKey: process.env.REACT_APP_API_KEY});
@@ -18,15 +22,17 @@ const Overlay = (props) => {
 
                 const directionsService = new google.maps.DirectionsService();
                 let response = directionsService.route({
-                    destination: "pennsylvania avenue residence hall",
-                    origin: "illinois street residence hall",
-                    travelMode: "WALKING"
+                    destination: "1002 W College Ct, Urbana, IL",
+                    origin: "201 N Goodwin Ave, Urbana, IL",
+                    travelMode: "DRIVING"
                 }, 
                     (results, status) => {
                         console.log(results);
                         console.log(status);
                         let location = `${props.countyName}, ${props.stateName}`;
-                        setImgUrl("https://maps.googleapis.com/maps/api/staticmap?center=" + location + "&zoom=10&size=400x400&key=" + process.env.REACT_APP_API_KEY);
+                        setImgUrl("https://maps.googleapis.com/maps/api/staticmap?center=" + location + "&zoom=10" +
+                        "&path=enc:" + results.routes[0].overview_polyline +
+                        "&size=400x400&key=" + process.env.REACT_APP_API_KEY);
                     }
                 );
             }
