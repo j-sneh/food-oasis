a5b792c72c89a9941046da9a90bad2416e970d28
Jonathan Sneh <jonathaninterweb@gmail.com>
2022-02-26 20:46:39 -0600

    Added Map + Overlay

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Map from './Map';
+
+// import BarChart from './BarChart';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Map></Map>
+            </div>
+        );
+    }
+}
+
+export default App;
